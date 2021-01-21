2962ae7caf94035d30a533168ef3dde197d00abc
MarchD <dubchmar@gmail.com>
2021-01-21 16:15:53 +0200

    Updates

---
+++ precache-manifest.0a448b4d625eaab3ad12e974e67ee1d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed6a56c97cf0177b8e4707fd198962c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8feae0ea2c581767cde",
+    "url": "./static/css/main.f7c650b4.chunk.css"
+  },
+  {
+    "revision": "88e4f816677f7c1c1b04",
+    "url": "./static/js/2.a8a88048.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a8a88048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8feae0ea2c581767cde",
+    "url": "./static/js/main.db82f1e5.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
